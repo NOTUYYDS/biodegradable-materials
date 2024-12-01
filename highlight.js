d6424a7ba531ad4e94eda22c4d5f521c4cd7a9aa
NOTUYYDS <cei113047@nptu.edu.tw>
2024-12-01 20:14:03 +0800

    Add files via upload

---
+++ highlight.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>語法高亮範例</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>程式碼高亮範例</h1>
+    <pre>
+<code>
+import pandas as pd
+import numpy as np
+
+# Sample comment
+data = [1, 2, 3, 4]
+</code>
+    </pre>
+    <script src="highlight.js"></script>
+</body>
+</html>
